f6fd8a8cc7f7b9aaad2fe5afafae9222a185150f
NoahGhost <thecodymaver@gmail.com>
2026-09-21 02:27:43 -0400

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,53 @@
+quiz = [
+    {
+        "question": "What is the capital of Florida?",
+        "choices": ["Tallahassee", "Miami", "Orlando", "Tampa"],
+        "answer": "Tallahassee"
+    },
+    {
+        "question": "What is the biggest planet?",
+        "choices": ["Earth", "Jupiter", "Saturn", "Mars"],
+        "answer": "Jupiter"
+    },
+    {
+        "question": "Who is the god of the sea?",
+        "choices": ["Poseidon", "Zeus", "Hades", "Apollo"],
+        "answer": "Poseidon"
+    },
+    {
+        "question": "What is the fastest land animal?",
+        "choices": ["Cheetah", "Lion", "Leopard", "Tiger"],
+        "answer": "Cheetah"
+    },
+    {
+        "question": "Who was the first Robin?",
+        "choices": ["Dick Grayson", "Jason Todd", "Tim Drake", "Damian Wayne"],
+        "answer": "Dick Grayson"
+    },
+    {
+        "question": "What is the capital of Japan?",
+        "choices": ["Seoul", "Beijing", "Tokyo", "Bangkok"],
+        "answer": "Tokyo"
+    },
+    {
+        "question": "Who is Voldemort?",
+        "choices": ["Tom Riddle", "Thomas Marvolo Riddle", "Mort", "Tom Marvolo Riddle"],
+        "answer": "Tom Marvolo Riddle"
+    }, 
+    {
+        "question": "What does the stupefy spell do?",
+        "choices": ["Stuns & knocks them unconscious", "Makes them stupid", "Makes them lose braincells", "Turns them into a statue"],
+        "answer": "Stuns & knocks them unconscious"
+    },
+    {
+        "question": "Who is the headmaster of Hogwarts?",
+        "choices": ["Albus Dumbledore", "Severus Snape", "Rubeus Hagrid", "Minerva McGonagall"],
+        "answer": "Albus Dumbledore"
+    },
+    {
+        "question": "Who says this phrase 'Demons I get. People are Crazy'?",
+        "choices": ["Dean Winchester", "Bobby Singer", "Lucifer", "Sam Winchester"],
+        "answer": "Dean Winchester"
+    }
+]
+    
